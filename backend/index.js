810a5e20e4a8cec503378bdf3c1bb7b70330667c
shreyam2004gupta <shreyamgupta437@gmail.com>
2026-05-23 23:11:33 +0530

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,28 @@
 import express from "express"
+import cors  from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import dns from 'dns';
+dns.setServers(["1.1.1.1","8.8.8.8"]);
 
-const app=express()
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database is connected")
+  })
+  .catch((err) => {
+    console.error("database not connected",err.message)
+  })
+
+const app = express()
+
+app.use(cors({
+    origin : process.env.FRONT_END_URL || "http://localhost:5173",
+    method:["GET","POST","PUT","DELETE"],
+    allowedHeaders:["Content-Type","Authorization"],
+}))
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log("server is running");
