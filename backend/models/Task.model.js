6014a8783a66e47e0a714a89ca9e8123a742cb7c
shreyam2004gupta <shreyamgupta437@gmail.com>
2026-05-23 23:38:21 +0530

    models

---
+++ backend/models/Task.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const TaskSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    discription:{
+        type:String,
+    },
+    priority:{
+        type:String,
+        enum:["Low","Medium","High"],
+        default:"Low",
+    },
+    status:{
+        type:String,
+        enum:["Pending","In Progress","Completed"],
+        default:"Pending",
+    },
+    dueDate:{
+        type:Date,
+        required:true,
+    },
+    assignedTo:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },],
+    createdBy:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },],
+    attachements:[
+        {
+            type:String,
+        },
+    ],
+    todoChecklist:[todoSchema],
+    progress:{type:Number, default:0},
+},
+{timestamps:true})
+
+const Task=mongoose.model("Task",TaskSchema)
+
+export default Task
+
